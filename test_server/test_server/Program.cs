e9d3f4b714a6e77ef4d93705c617c59941c2809e
Picard Xie <picardxie@foxmail.com>
2017-05-24 10:45:52 +0800

    Add files via upload

---
+++ test_server/test_server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using ShareUtil;
+namespace test_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Log初始化
+            LogConfig config = new LogConfig();
+            config.logLevel = 1;
+            config.isConsole = true;
+            config.isFile = false;
+            config.filePath = "./pig.log";
+            Log.init(config);
+            #endregion
+            #region server初始化
+            TcpServer tcpServer = new TcpServer();
+            tcpServer.Start();
+            #endregion
+            string key = Console.ReadLine();
+            while (key != "q") {
+                key = Console.ReadLine();
+            }
+            tcpServer.Stop();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ test_server/test_server/Server/TcpServer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using ShareUtil;
+namespace test_server
+{
+    class TcpServer
+    {
+        protected TcpListener tcpListener;
+        protected Thread m_thread;
+        protected bool stillListen;
+        protected AsyncCallback acceptCallBack;
+        protected AutoResetEvent acceptEvent;
+        public TcpServer(){
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            tcpListener = new TcpListener(ip,8080);
+            acceptCallBack = new AsyncCallback(AcceptCallBack);
+            acceptEvent = new AutoResetEvent(true);
+        }
+        #region public接口
+        public void Start() {
+            stillListen = true;
+            if (m_thread == null) {
+                m_thread = new Thread(MainProc);
+                m_thread.Start();
+            }
+        }
+        public void Stop() {
+            stillListen = false;
+            if (m_thread != null) {
+                m_thread.Join();
+                m_thread = null;
+                Log.Info("process is end");
+            }
+        }
+        #endregion
+        #region private接口
+        protected void OnAccept(Socket socket) {
+            Log.Info("i am receiving a socket and port num is "+ ((IPEndPoint)socket.RemoteEndPoint).Port.ToString());
+        }
+        protected void StartListen() {
+            tcpListener.Start();
+        }
+        protected void Accept() {
+            if (tcpListener == null) {
+                return;
+            }
+            if (acceptEvent.WaitOne(200)) {
+                tcpListener.BeginAcceptSocket(acceptCallBack, tcpListener);
+            }
+        }
+        protected void AcceptCallBack(IAsyncResult result) {
+            Socket socket = tcpListener.EndAcceptSocket(result);
+            OnAccept(socket);
+            acceptEvent.Set();
+        }
+        protected void MainProc()
+        {
+            Log.Info("process is on");
+            while (stillListen)
+            {
+                StartListen();
+                Accept();
+            }
+        }
+        #endregion
+    }
+}
